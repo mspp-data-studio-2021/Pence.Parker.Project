aa32e39aca76c320de616179da8f27b506de87bb
ppence17 <pwp2440@nyu.edu>
2021-07-12 16:45:07 -0400

    reorg without "raw"

--- r/01_download_cc_districts.R
+++ r/01_download_cc_districts.R
@@ -2,15 +2,15 @@ library(fs)
 library(zip)
 
 file_url <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nycc_21b.zip"
-zip_file <- path("raw", "nycc_21b")
-shp_file <- path("raw", "nycc.shp")
+zip_file <- path("data", "nyccitycouncildistricts_21b.zip")
+shp_file <- path("data", "nycc.shp")
 
 if (!file_exists(zip_file) & !file_exists(shp_file)) {
   download.file(file_url, zip_file, mode = "wb")
 }
 
 if (!file_exists(shp_file)) {
-  unzip(zip_file, exdir = path("raw", "nycc_21b"))
+  unzip(zip_file, exdir = path("data"))
 }
 
 file_delete(zip_file)
