55a60934892470b3d9d48d975be3a114fcee4ae8
Parker Pence <pwp2440@nyu.edu>
2021-07-27 11:52:02 -0500

    Add school districts

--- r/01_download_cc_districts.R
+++ r/01_download_cc_districts.R
@@ -1,6 +1,7 @@
 library(fs) 
 library(zip)
 
+#Download City Council Districts
 file_url <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nycc_21b.zip"
 zip_file <- path("data", "nyccitycouncildistricts_21b.zip")
 shp_file <- path("data", "nycc.shp")
@@ -15,10 +16,10 @@ if (!file_exists(shp_file)) {
 
 file_delete(zip_file)
 
-
-file_url <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nychdb_inactiveincluded_20q4_csv.zip?r=a"
-zip_file <- path("data", "_")
-csv_file <- path("data", "_")
+#Download School Districts
+file_url <- "https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nysd_21b.zip"
+zip_file <- path("data", "nysd_21b.zip")
+csv_file <- path("data", "nysd.shp")
 
 if (!file_exists(zip_file) & !file_exists(csv_file)) {
   download.file(file_url, zip_file, mode = "wb")
